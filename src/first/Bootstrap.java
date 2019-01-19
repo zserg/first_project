10f69f747a6ed0954fe4aaa89e610ae7f2dcff45
Sergey Zadyabin <zsergt3@gmail.com>
2019-01-19 22:44:20 +0300

    add the first unit test

--- src/first/Bootstrap.java
+++ src/first/Bootstrap.java
@@ -1,4 +1,4 @@
-package mytest;
+package first;
 
 public class Bootstrap {
 
---
+++ tests/first/BootstrapTest.java
@@ -0,0 +1,12 @@
+package first;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class BootstrapTest {
+
+    @Test
+    public void testHttpServerReady() {
+        assertTrue(false);
+    }
+}
