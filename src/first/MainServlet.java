f6730eb11fd695d12d784a8ddc17d877bbaacd74
zserg <zsergt3@gmail.com>
2019-01-28 18:20:19 +0300

    add form

--- src/first/MainServlet.java
+++ src/first/MainServlet.java
@@ -1,19 +1,26 @@
 package first;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 
 public class MainServlet extends HttpServlet {
 
         @Override
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            PrintWriter pw = response.getWriter();
+
             response.setContentType("text/html");
             response.setStatus(HttpServletResponse.SC_OK);
-            response.getWriter().println("<h1>Hello SimpleServlet</h1>");
+
+            RequestDispatcher dispatcher
+                  = request.getRequestDispatcher("index.jsp");
+           dispatcher.forward(request, response);
         }
 }
 
---
+++ src/first/index.jsp
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>XML Converter</title>
+</head>
+<body>
+
+<form action="convert">
+  <textarea name="message" rows="10" cols="30">The cat was playing in the garden.</textarea>
+  <br>
+  <input type=\"submit\">
+</form>
+
+</body>
+</html>
+
